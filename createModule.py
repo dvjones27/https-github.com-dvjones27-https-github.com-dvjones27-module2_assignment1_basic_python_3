a32125a3660aabd4eb066206f99519588eb2dbc2
dvjones27 <102387309+dvjones27@users.noreply.github.com>
2022-10-24 16:41:33 -0400

    Module 2 Assignment 3 Basic Python First Commit

---
+++ createModule.py
@@ -0,0 +1,14 @@
+from math import pi
+
+def sqFootage(length, width):
+    length = int(input("Enter the length of the house: "))
+    width = int(input("Enter the width of the house: "))
+    print (f"The area of the house is {length * width}.")
+
+
+
+
+def circumference(radius):
+    radius = int(input("Input the radius of the circle: "))
+    print(f"The radius of the circle is {2 * pi * radius}.")
+    
